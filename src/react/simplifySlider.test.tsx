1acf6f9a326637481023d48982ac3c13a42ee5d1
SeoJaeWan <sjw7324@igis.co.kr>
2025-04-09 09:44:39 +0900

    init: 테스트 오류 문구 수정

--- src/react/simplifySlider.test.tsx
+++ src/react/simplifySlider.test.tsx
@@ -1,5 +1,9 @@
 describe("Simplify Slider 테스트", () => {
-  //   it("성공", () => {
-  //     expect(true).toBe(true);
-  //   });
+  it("실패 1", () => {
+    expect(true).toBe(true);
+  });
+
+  it("실패 2", () => {
+    expect(true).toBe(true);
+  });
 });
