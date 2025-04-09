0c068df75ececc2ccb89d56ba474127cd5a0b69c
SeoJaeWan <sjw7324@igis.co.kr>
2025-04-09 09:45:40 +0900

    chore: 테스트 실패 추가

--- src/react/simplifySlider.test.tsx
+++ src/react/simplifySlider.test.tsx
@@ -1,9 +1,9 @@
 describe("Simplify Slider 테스트", () => {
   it("실패 1", () => {
-    expect(true).toBe(true);
+    expect(false).toBe(true);
   });
 
   it("실패 2", () => {
-    expect(true).toBe(true);
+    expect(false).toBe(true);
   });
 });
