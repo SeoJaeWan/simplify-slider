d24fc3ec007ac9730bfb3a4c8b2b846b87b952eb
SeoJaeWan <sjw7324@naver.com>
2025-04-09 20:31:16 +0900

    init: 테스트 케이스 성공으로 변경

--- src/react/simplifySlider.test.tsx
+++ src/react/simplifySlider.test.tsx
@@ -1,9 +1,5 @@
 describe("Simplify Slider 테스트", () => {
-  it("실패 1", () => {
-    expect(false).toBe(true);
-  });
-
-  it("실패 2", () => {
-    expect(false).toBe(true);
+  it("성공", () => {
+    expect(true).toBe(true);
   });
 });
