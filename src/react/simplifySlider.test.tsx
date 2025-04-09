947198033727d86f66bd10e21a38b22cdc0cad07
SeoJaeWan <sjw7324@naver.com>
2025-04-09 21:38:03 +0900

    save

--- src/react/simplifySlider.test.tsx
+++ src/react/simplifySlider.test.tsx
@@ -1,5 +1,18 @@
+import { render } from "@testing-library/react";
+import { SimplifySlider } from "./simplifySlider";
+
 describe("Simplify Slider 테스트", () => {
-  it("성공", () => {
-    expect(true).toBe(true);
+  it("ReactDOM을 children으로 전달받으면 Simplify Slider와 함께 렌더링된다.", () => {
+    const { container, getByText } = render(
+      <SimplifySlider>
+        <div>Test</div>
+      </SimplifySlider>,
+    );
+
+    const slider = container.querySelector(".simplify-slider");
+    expect(slider).toBeInTheDocument();
+
+    const children = getByText("Test");
+    expect(children).toBeInTheDocument();
   });
 });
--- src/react/simplifySlider.tsx
+++ src/react/simplifySlider.tsx
@@ -1,9 +1,15 @@
-export interface SimplifySliderProps {
-  pagination: boolean;
+import { PropsWithChildren } from "react";
+
+export interface SimplifySliderProps extends PropsWithChildren {
+  pagination?: boolean;
 }
 
-const SimplifySlider: React.FC<SimplifySliderProps> = () => {
-  return <></>;
+const SimplifySlider: React.FC<SimplifySliderProps> = (props) => {
+  const { children, pagination } = props;
+
+  console.error(pagination);
+
+  return <div>{children}</div>;
 };
 
 export { SimplifySlider };
