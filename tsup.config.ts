dc8ec907522b30cd023bd2d4f3c2499e6dfd5cd8
SeoJaeWan <sjw7324@igis.co.kr>
2025-04-15 15:45:13 +0900

    fix: size limit 테스트

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,9 +5,7 @@ export default defineConfig({
     "react/index": "src/react/index.ts",
   },
   format: ["cjs", "esm"],
-  dts: {
-    resolve: false,
-  },
+  dts: true,
   sourcemap: true,
   clean: true,
   external: ["react", "react-dom"],
