abb1405736a9dea9e6640d8767f9cf2382f533b0
SeoJaeWan <sjw7324@naver.com>
2025-04-15 22:29:30 +0900

    feat: 실제 컴포넌트에서 테스트 진행

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,37 +1,17 @@
-import { useRef } from "react";
-import { SimplifySlide, SimplifySlider } from "simplify-slider/react";
-import type { Simplify } from "simplify-slider/react";
+import { SimplifySlide, SimplifySlider, useSimplifySlide } from "simplify-slider/react";
 
 function App() {
-  const slide = useRef<Simplify>(null);
-
-  const handleNext = () => {
-    if (slide.current) {
-      slide.current.next();
-    }
-  };
-
-  const handlePrev = () => {
-    if (slide.current) {
-      slide.current.prev();
-    }
-  };
-
-  const handleGoTo = (index: number) => {
-    if (slide.current) {
-      slide.current.goTo(index);
-    }
-  };
+  const { core, next, prev, goTo } = useSimplifySlide();
 
   return (
     <SimplifySlider
-      ref={slide}
+      ref={core}
       options={{
         loop: true,
         drag: true,
         duration: 1000,
         autoplay: {
-          enabled: false,
+          enabled: true,
           interval: 1000,
           direction: "right",
         },
@@ -47,14 +27,18 @@ function App() {
         <div style={{ height: "200px" }}>3</div>
       </SimplifySlide>
 
-      <button onClick={handlePrev}>이전</button>
-      <button onClick={handleNext}>다음</button>
+      <button onClick={prev}>이전</button>
+      <button onClick={next}>다음</button>
 
       <div>
         <li>
-          <button onClick={() => handleGoTo(1)}>1</button>
-          <button onClick={() => handleGoTo(2)}>2</button>
-          <button onClick={() => handleGoTo(3)}>3</button>
+          <button onClick={() => goTo(1)}>1</button>
+        </li>
+        <li>
+          <button onClick={() => goTo(2)}>2</button>
+        </li>
+        <li>
+          <button onClick={() => goTo(3)}>3</button>
         </li>
       </div>
     </SimplifySlider>
