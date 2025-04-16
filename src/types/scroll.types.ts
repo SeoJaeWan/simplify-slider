3dd260a6f302df46cb6c7310314508aa89197426
SeoJaeWan <sjw7324@naver.com>
2025-04-16 22:16:56 +0900

    refactor: jsdoc 설명 수정

--- src/types/scroll.types.ts
+++ src/types/scroll.types.ts
@@ -1,13 +1,21 @@
 /**
+ * ScrollOptions interface
+ */
+
+/**
  * @typedef {Object} ScrollOptions
  * @property {boolean} loop - Whether to loop the slides or not. Default is false.
  * @property {boolean} drag - Whether to enable drag functionality. Default is false.
  * @property {number} duration - Duration of the transition in milliseconds. Default is 500.
  */
 export interface ScrollOptions {
+  /** Whether to loop the slides or not. Default is false. */
   loop: boolean;
+  /** Duration of the transition in milliseconds. Default is 500. */
   duration: number;
+  /** Whether to enable drag functionality. Default is false. */
   drag: boolean;
+  /** Whether to enable autoplay functionality. Default is false. */
   autoplay?: AutoplayOptions;
 }
 
@@ -17,12 +25,18 @@ export interface ScrollOptions {
  * @property {"right" | "left"} direction - Direction of autoplay. Default is "right".
  */
 export interface AutoplayOptions {
+  /** Interval between slides in milliseconds. Default is 3000. */
   interval: number;
-  direction: "right" | "left";
+  /** Direction of autoplay. Default is "right". */
+  direction: Direction;
+  /** Whether to roll the slides or not. Default is false. */
   rolling: boolean;
+  /** Callback function to be called on progress. */
   onProgress: (progress: number) => void;
 }
 
 export interface SimplifySliderOptions extends Partial<Omit<ScrollOptions, "autoplay">> {
   autoplay?: Partial<AutoplayOptions>;
 }
+
+export type Direction = "right" | "left";
